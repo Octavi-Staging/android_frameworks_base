76e017ea9c0020287322ad75157fb9e9448a7a62
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2)
    Squashed:
    Keyguard: Don't disable visibility when scrambling pin.
    Keyguard: Fix scramblepin logic.
    SystemUI: Read PIN scramble setting from current user
    
    Change-Id: I6b3763c05f8dc6d49c62c5dc5c01dce8d18d09ca
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
@@ -16,7 +16,10 @@
 
 package com.android.keyguard;
 
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.android.internal.util.LatencyTracker;
 import com.android.internal.widget.LockPatternUtils;
@@ -26,6 +29,11 @@ import com.android.systemui.classifier.FalsingCollector;
 import com.android.systemui.flags.FeatureFlags;
 import com.android.systemui.flags.Flags;
 import com.android.systemui.statusbar.policy.DevicePostureController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class KeyguardPinViewController
         extends KeyguardPinBasedInputViewController<KeyguardPINView> {
@@ -43,6 +51,8 @@ public class KeyguardPinViewController
     private long mPinLength;
 
     private boolean mDisabledAutoConfirmation;
+
+    private static List<Integer> sNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
 
     protected KeyguardPinViewController(KeyguardPINView view,
             KeyguardUpdateMonitor keyguardUpdateMonitor,
@@ -76,7 +86,38 @@ public class KeyguardPinViewController
                 getKeyguardSecurityCallback().onCancelClicked();
             });
         }
+
         mPasswordEntry.setUserActivityListener(this::onUserInput);
+
+        boolean scramblePin = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0,
+                UserHandle.USER_CURRENT) == 1;
+
+        if (scramblePin) {
+            Collections.shuffle(sNumbers);
+            // get all children who are NumPadKey's
+            LinearLayout container = (LinearLayout) mView.findViewById(R.id.pin_container);
+
+            List<NumPadKey> views = new ArrayList<NumPadKey>();
+            for (int i = 0; i < container.getChildCount(); i++) {
+                if (container.getChildAt(i) instanceof LinearLayout) {
+                    LinearLayout nestedLayout = ((LinearLayout) container.getChildAt(i));
+                    for (int j = 0; j < nestedLayout.getChildCount(); j++){
+                        View view = nestedLayout.getChildAt(j);
+                        if (view.getClass() == NumPadKey.class) {
+                            views.add((NumPadKey) view);
+                        }
+                    }
+                }
+            }
+
+            // reset the digits in the views
+            for (int i = 0; i < sNumbers.size(); i++) {
+                NumPadKey view = views.get(i);
+                view.setDigit(sNumbers.get(i));
+            }
+        }
+
         mPostureController.addCallback(mPostureCallback);
     }
 
