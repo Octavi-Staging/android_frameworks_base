5ca94db4508b6ff6071bf6c13debe2c0b42aad32
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    [1/2] base: allow disable of screenshot shutter sound
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -1088,7 +1088,11 @@ public class ScreenshotController {
      */
     private void saveScreenshotAndToast(UserHandle owner, Consumer<Uri> finisher) {
         // Play the shutter sound to notify that we've taken a screenshot
-        playCameraSound();
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+                // Play the shutter sound to notify that we've taken a screenshot
+                playCameraSound();
+         }
 
         saveScreenshotInWorkerThread(
                 owner,
@@ -1131,8 +1135,11 @@ public class ScreenshotController {
             });
         }
 
-        // Play the shutter sound to notify that we've taken a screenshot
-        playCameraSound();
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+            // Play the shutter sound to notify that we've taken a screenshot
+            playCameraSound();
+        }
 
         if (DEBUG_ANIM) {
             Log.d(TAG, "starting post-screenshot animation");
