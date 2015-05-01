fb551b947d90698a2cff4fb694bd433c3ecdea69
Christopher R. Palmer <crpalmer@gmail.com>
2015-05-01 06:15:47 -0400

    DreamManager: Expose doze state
    Previously the dream service would only report whether or not it was dreaming.
    Ambient display counts as dreaming.  When dreaming, the volume inputs are
    passed back to the application which is not what we want for ambient
    display.
    
    Fix this by exposing isDozing() and allow volume keys to control music
    when we are dreaming and have moved into doze mode (aka, sleeping).
    
    Change-Id: I3b1e72b95ec8b1b63ef0219259dfee77a5d339d3
    (cherry picked from commit 579f24d427758569a023650bdbe51fca290fedd6)
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4426,7 +4426,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             IDreamManager dreamManager = getDreamManager();
 
             try {
-                if (dreamManager != null && dreamManager.isDreaming()) {
+                if (dreamManager != null && dreamManager.isDreaming() && !dreamManager.isDozing()) {
                     return true;
                 }
             } catch (RemoteException e) {
