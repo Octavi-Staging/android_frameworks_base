5d18b6b8506bf16ead3f05297e7bd90a698e8c1d
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Author: Keith Mok <kmok@cyngn.com>
    Date:   Thu Feb 4 11:10:22 2016 -0800
    
        SystemUI: Add caffeine qs tile
    
        Change-Id: I5d4aa97ae63fc0bcc40f0fa7d9305efbba091029
    
    Author: chillger <alexander.loskutov@gmail.com>
    Date:   Fri May 22 13:16:00 2020 +0200
    
        SystemUI: Long press Caffeine for infinite duration
    
        Change-Id: Ic49539dcda6d7d6b1dab362eb8ec168d2ec65b07
    
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332

--- packages/SystemUI/src/com/android/systemui/octavi/OctaviModule.kt
+++ packages/SystemUI/src/com/android/systemui/octavi/OctaviModule.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.octavi
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.CaffeineTile
 
 import dagger.Binds
 import dagger.Module
@@ -25,4 +26,9 @@ import dagger.multibindings.StringKey
 
 @Module
 interface OctaviModule {
+    /** Inject CaffeineTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CaffeineTile.TILE_SPEC)
+    fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
 }
