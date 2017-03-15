438e374acee715a9fd9a7fe5a7e1d506822dc050
Pranav Vashi <neobuddy89@gmail.com>
2017-03-15 07:36:19 +0530

    SystemUI: Make roaming indicator optional [1/2]
    * Roaming indicator on signal icon is useless most of the times
      since a carrier might not charge differently to user if in same
      country.
    * So, allow user to hide roaming indicator.
    * This commit will only allow to hide indicator not roaming warnings.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5841,6 +5841,12 @@ public final class Settings {
         public static final String EDGE_LIGHT_CUSTOM_COLOR = "edge_light_custom_color";
 
         /**
+         * Whether to display roaming indicator for roaming signal connection
+         * @hide
+         */
+        public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
