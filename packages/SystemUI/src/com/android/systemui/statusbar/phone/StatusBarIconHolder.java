558225de99b4365aaf8da6ad12c1bca4394a42e5
SagarMakhar <sagarmakhar@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Introduce dynamic VoLTE & VoWiFi icons
    @neobuddy89: Clean up unrequired derps. Adapted for A13 QPR1.
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Lostark13 <tushar9915@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconHolder.java
@@ -23,6 +23,7 @@ import android.graphics.drawable.Icon;
 import android.os.UserHandle;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.systemui.statusbar.connectivity.ImsIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.CallIndicatorIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.MobileIconState;
 import com.android.systemui.statusbar.phone.StatusBarSignalPolicy.WifiIconState;
@@ -38,6 +39,7 @@ public class StatusBarIconHolder {
     public static final int TYPE_ICON = 0;
     public static final int TYPE_WIFI = 1;
     public static final int TYPE_MOBILE = 2;
+
     /**
      * TODO (b/249790733): address this once the new pipeline is in place
      * This type exists so that the new pipeline (see {@link MobileIconViewModel}) can be used
@@ -62,13 +64,15 @@ public class StatusBarIconHolder {
      */
     @Deprecated
     public static final int TYPE_WIFI_NEW = 4;
+    public static final int TYPE_IMS = 5;
 
     @IntDef({
             TYPE_ICON,
             TYPE_WIFI,
             TYPE_MOBILE,
             TYPE_MOBILE_NEW,
-            TYPE_WIFI_NEW
+            TYPE_WIFI_NEW,
+            TYPE_IMS
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface IconType {}
@@ -76,6 +80,7 @@ public class StatusBarIconHolder {
     private StatusBarIcon mIcon;
     private WifiIconState mWifiState;
     private MobileIconState mMobileState;
+    private ImsIconState mImsState;
     private @IconType int mType = TYPE_ICON;
     private int mTag = 0;
 
@@ -109,6 +114,13 @@ public class StatusBarIconHolder {
         StatusBarIconHolder holder = new StatusBarIconHolder();
         holder.mIcon = new StatusBarIcon(UserHandle.SYSTEM, context.getPackageName(),
                 Icon.createWithResource( context, resId), 0, 0, contentDescription);
+        return holder;
+    }
+
+    public static StatusBarIconHolder fromImsIconState(ImsIconState state) {
+        StatusBarIconHolder holder = new StatusBarIconHolder();
+        holder.mImsState = state;
+        holder.mType = TYPE_IMS;
         return holder;
     }
 
@@ -195,6 +207,14 @@ public class StatusBarIconHolder {
         mMobileState = state;
     }
 
+    public ImsIconState getImsState() {
+        return mImsState;
+    }
+
+    public void setImsState(ImsIconState state) {
+        mImsState = state;
+    }
+
     public boolean isVisible() {
         switch (mType) {
             case TYPE_ICON:
@@ -208,8 +228,10 @@ public class StatusBarIconHolder {
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // this is effectively an unused return value.
                 return true;
-            default:
-                return true;
+            case TYPE_IMS:
+                return mImsState.visible;
+
+            default: return true;
         }
     }
 
@@ -235,6 +257,9 @@ public class StatusBarIconHolder {
             case TYPE_WIFI_NEW:
                 // The new pipeline controls visibilities via the view model and view binder, so
                 // ignore setVisible.
+                break;
+            case TYPE_IMS:
+                mImsState.visible = visible;
                 break;
         }
     }
