d46a5612a52dc790f58984e9c2f006a71fc4945c
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-28 16:09:16 +0800

    base: QS tiles layout customizations [1/2]
    - Inspired from https://github.com/omnirom/android_frameworks_base/commit/4d71992f4446d828e0f5e7eb3841632ea28ee71c.
      To avoid breaking aosp performance of calculating stuff here and there, use overlay to implement this.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- core/java/com/android/internal/util/octavi/OctaviUtils.java
+++ core/java/com/android/internal/util/octavi/OctaviUtils.java
@@ -19,6 +19,7 @@ package com.android.internal.util.octavi;
 import android.Manifest;
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.om.IOverlayManager;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
 import android.hardware.input.InputManager;
@@ -237,6 +238,34 @@ public class OctaviUtils {
             return Settings.System.getIntForUser(context.getContentResolver(),
                     Settings.System.QS_TILE_VERTICAL_LAYOUT,
                     0, UserHandle.USER_CURRENT) == 1;
+        }
+
+        public static boolean updateLayout(Context context) {
+            final IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(ServiceManager.getService(
+                    Context.OVERLAY_SERVICE));
+            final int layout_qs = Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QS_LAYOUT,
+                    42, UserHandle.USER_CURRENT);
+            final int layout_qqs = Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QQS_LAYOUT,
+                    22, UserHandle.USER_CURRENT);
+            final int row_qs = layout_qs / 10;
+            final int col_qs = layout_qs % 10;
+            final int row_qqs = layout_qqs / 10;
+            for (int i = 0; i < 2; ++i) {
+                String pkgName;
+                if (i == 0) {
+                    pkgName = String.format("com.octavi.qs.portrait.layout_%sx%s", Integer.toString(row_qs), Integer.toString(col_qs));
+                } else {
+                    pkgName = String.format("com.octavi.qqs.portrait.layout_%sx%s", Integer.toString(row_qqs), Integer.toString(col_qs));
+                }
+                try {
+                    overlayManager.setEnabledExclusiveInCategory(pkgName, UserHandle.USER_CURRENT);
+                } catch (RemoteException re) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
