2e54c77e626f62b741cf9a21c622cfb49d0476f3
althafvly <althafvly@gmail.com>
2022-07-11 15:13:40 +0530

    SystemUI: Redo circle battery style
    - Now shows up on QuickSettings panel.
    - Fixes issues with battery circle style on theme and
      ui mode change and statusbar icon toggle.
    - Fixes wrong tint color with circle battery icon.
    - Re-evaluating system theme to avoid delay in circle
      battery tint color change.
    
    Change-Id: Id90f85e47ce4b9811e9bb67b91169997020300d8

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterViewController.java
@@ -16,6 +16,7 @@
 package com.android.systemui.battery;
 
 import static android.provider.Settings.Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME;
+import static android.provider.Settings.System.STATUS_BAR_BATTERY_STYLE;
 import static android.provider.Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT;
 
 import android.content.ContentResolver;
@@ -71,10 +72,8 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
                         getContext(), newValue);
                 mBatteryHidden = icons.contains(mSlotBattery);
                 mView.setVisibility(mBatteryHidden ? View.GONE : View.VISIBLE);
-            } else if (BatteryMeterView.STATUS_BAR_BATTERY_STYLE.equals(key)) {
                 if (!mBatteryHidden) {
-                    mView.setBatteryStyle(TunerService.parseInteger(newValue,
-                            BatteryMeterView.BATTERY_STYLE_PORTRAIT));
+                    mView.updateBatteryStyle();
                 }
             }
         }
@@ -181,8 +180,7 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
             return;
         }
 
-        mTunerService.addTunable(mTunable, StatusBarIconController.ICON_HIDE_LIST,
-                BatteryMeterView.STATUS_BAR_BATTERY_STYLE);
+        mTunerService.addTunable(mTunable, StatusBarIconController.ICON_HIDE_LIST);
         mIsSubscribedForTunerUpdates = true;
     }
 
@@ -198,6 +196,11 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
     private void registerShowBatteryPercentObserver(int user) {
         mContentResolver.registerContentObserver(
                 Settings.System.getUriFor(STATUS_BAR_SHOW_BATTERY_PERCENT),
+                false,
+                mSettingObserver,
+                user);
+        mContentResolver.registerContentObserver(
+                Settings.System.getUriFor(STATUS_BAR_BATTERY_STYLE),
                 false,
                 mSettingObserver,
                 user);
@@ -223,6 +226,9 @@ public class BatteryMeterViewController extends ViewController<BatteryMeterView>
                     Settings.Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME)) {
                 // update the text for sure if the estimate in the cache was updated
                 mView.updatePercentText();
+            }
+            if (TextUtils.equals(uri.getLastPathSegment(), STATUS_BAR_BATTERY_STYLE)) {
+                mView.updateBatteryStyle();
             }
         }
     }
