picasso09/android_frameworks_base-nusa

SystemUI: Hide oos clear all if reticker enable

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -5256,6 +5256,7 @@ public class NotificationPanelViewController extends PanelViewController {
         boolean debug = true;
         if (visibility && getExpandedFraction() != 1) {
             mNotificationStackScroller.setVisibility(GONE);
+            mStatusBar.updateDismissAllVisibility(false);
             ExpandableNotificationRow row = mHeadsUpManager.getTopEntry().getRow();
             String pkgname = row.getEntry().getSbn().getPackageName();
             Drawable icon = null;

Octavi-Staging/android_frameworks_base

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -6533,6 +6533,7 @@ public final class NotificationPanelViewController implements Dumpable {
             StatusBarNotification sbn = mHeadsUpManager.getTopEntry().getRow().getEntry().getSbn();
             Notification notification = sbn.getNotification();
             String pkgname = sbn.getPackageName();
+            mCentralSurfaces.updateDismissAllVisibility(false);
             Drawable icon = null;
             try {
                 if (pkgname.equals("com.android.systemui")) {