9c35593413b7026a069990a20ceef923a6ea0508
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-18 15:47:29 +0200

    SystemUI: Adapt circle battery background to white/dark QS changes
    Just reduce the f/g alpha to make the b/g. This should always work
    Also apply to new header.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -385,13 +385,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                 mTintedIconManager.setTint(textColor);
             }
             if (mBatteryRemainingIcon.getBatteryStyle() == BATTERY_STYLE_CIRCLE) {
-                final float factor = 0.3f;
-                textColorSecondary = Color.argb(
-                    Color.alpha(textColor),
-                    Math.round(Color.red(textColor) * factor),
-                    Math.round(Color.green(textColor) * factor),
-                    Math.round(Color.blue(textColor) * factor)
-                );
+                textColorSecondary = reduceColorAlpha(textColor, 0.3f);
             }
             mBatteryRemainingIcon.updateColors(mTextColorPrimary, textColorSecondary,
                     mTextColorPrimary);
@@ -415,6 +409,14 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         updateAnimators();
 
         updateClockDatePadding();
+    }
+
+    private static int reduceColorAlpha(int color, float factor) {
+        final int a = Math.round(Color.alpha(color) * factor);
+        final int r = Color.red(color);
+        final int g = Color.green(color);
+        final int b = Color.blue(color);
+        return Color.argb(a, r, g, b);
     }
 
     private void updateClockDatePadding() {
