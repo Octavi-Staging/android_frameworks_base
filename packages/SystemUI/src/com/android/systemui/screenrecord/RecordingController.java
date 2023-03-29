a5b84071852d8e24912e392cdf5865bd1cd47338
Lostark13 <tushar9915@gmail.com>
2023-03-29 07:58:00 +0000

    ScreenRecord: Prevent NPE for External ScreenRecording usage

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingController.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingController.java
@@ -136,12 +136,15 @@ public class RecordingController
                                            DialogLaunchAnimator dialogLaunchAnimator,
                                            ActivityStarter activityStarter,
                                            @Nullable Runnable onStartRecordingClicked) {
+        if (flags == null) {
+            return new ScreenRecordDialog(context, this, activityStarter,
+                mUserContextProvider, flags, dialogLaunchAnimator, onStartRecordingClicked);
+        }
         if (mFlags.isEnabled(Flags.WM_ENABLE_PARTIAL_SCREEN_SHARING_ENTERPRISE_POLICIES)
                 && mDevicePolicyResolver.get()
                         .isScreenCaptureCompletelyDisabled(getHostUserHandle())) {
             return new ScreenCaptureDisabledDialog(mContext);
         }
-
         return flags.isEnabled(Flags.WM_ENABLE_PARTIAL_SCREEN_SHARING)
                 ? new ScreenRecordPermissionDialog(context,  getHostUserHandle(), this,
                     activityStarter, dialogLaunchAnimator, mUserContextProvider,
