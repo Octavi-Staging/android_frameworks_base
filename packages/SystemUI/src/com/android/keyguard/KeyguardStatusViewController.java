cd9fa0625e68e510c116d3badb8ac742b7c4c734
Lostark13 <tushar9915@gmail.com>
2023-04-18 12:44:28 +0000

    Revert "SystemUI: Match lockscreen clock font with system"
    This reverts commit 9d3a25fbf5b03b49f4c4007bb8a05d32a18d56b8.

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
@@ -207,11 +207,6 @@ public class KeyguardStatusViewController extends ViewController<KeyguardStatusV
         public void onDensityOrFontScaleChanged() {
             mKeyguardClockSwitchController.onDensityOrFontScaleChanged();
         }
-
-        @Override
-        public void onThemeChanged() {
-            mKeyguardClockSwitchController.onDensityOrFontScaleChanged();
-        }
     };
 
     private KeyguardUpdateMonitorCallback mInfoCallback = new KeyguardUpdateMonitorCallback() {
