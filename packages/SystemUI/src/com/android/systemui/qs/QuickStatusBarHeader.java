476e84b673b2673448fe60a4e9bd2a4f78fadba5
Col_or <color597@outlook.com>
2023-06-22 10:10:49 +0000

    fixup- SystemUI: Match collapsed qs header height with status bar

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -26,6 +26,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import com.android.internal.policy.SystemBarUtils;
 import com.android.systemui.R;
 import com.android.systemui.util.LargeScreenUtils;
 
